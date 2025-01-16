8df26ebed520d8fc16fa76082d085cb7eee3f67e
Andrew Reeves <areeves42@gmail.com>
2025-01-16 12:44:12 -0500

    Wrap capture in a mutex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,36 +32,49 @@ export default function Home() {
     };
   }, []);
 
+  // mutex to prevent double-tap triggering multiple captures at once
+  const isCapturing  = useRef(false);
   async function onCapture() {
-    setDescription('(Capturing...)');
-    const img = imgRef.current;
-    if(!img) {
-      throw new Error('imgRef is null');
-    }
-    img.src = '';
-    const v = videoRef.current;
-    if(!v) {
-      throw new Error('videoRef null');
-    }
-    const c = document.createElement('canvas');
-    c.width = v.videoWidth;
-    c.height = v.videoHeight;
-    const ctx = c.getContext('2d');
-    if(!ctx) {
-      throw new Error('Unable to get 2d context for canvas');
-    }
-    ctx.drawImage(v, 0, 0);
-    const dataUrl = c.toDataURL();
-    img.src = dataUrl;
+    if(isCapturing.current) return;
+    isCapturing.current = true; // set back to false in finally
+    try {
+      setDescription('(Capturing...)');
+      const img = imgRef.current;
+      if(!img) {
+        throw new Error('imgRef is null');
+      }
+      img.src = '';
+      const v = videoRef.current;
+      if(!v) {
+        throw new Error('videoRef null');
+      }
+      const c = document.createElement('canvas');
+      c.width = v.videoWidth;
+      c.height = v.videoHeight;
+      const ctx = c.getContext('2d');
+      if(!ctx) {
+        throw new Error('Unable to get 2d context for canvas');
+      }
+      ctx.drawImage(v, 0, 0);
+      const dataUrl = c.toDataURL();
+      img.src = dataUrl;
 
-    setDescription('(Generating...)');
-    const client = new OpenAI({ apiKey: apikey, dangerouslyAllowBrowser: true });
-    const aiResponse = await classifyImage(client, dataUrl );
-    const newDescription = aiResponse.content ?? '';
-    setDescription(newDescription);
-    const speech = new SpeechSynthesisUtterance(newDescription);
-    speechSynthesis.speak(speech);
-
+      setDescription('(Generating...)');
+      const client = new OpenAI({ apiKey: apikey, dangerouslyAllowBrowser: true });
+      const aiResponse = await classifyImage(client, dataUrl );
+      const newDescription = aiResponse.content ?? '';
+      setDescription(newDescription);
+      await new Promise( (resolve, reject) => {
+        const utterance = new SpeechSynthesisUtterance(newDescription);
+        utterance.onend = resolve;
+        utterance.onerror = reject;
+        speechSynthesis.speak(utterance);
+      });
+    } catch(error) {
+      console.error('Error occurred in onCapture()', error);
+    } finally {
+      isCapturing.current = false;
+    }
   }
 
   return (
